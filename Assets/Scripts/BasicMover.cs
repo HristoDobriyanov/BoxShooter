1be9be807f9318bf0d27284a892c01dab32e8c32
Hristo Dobriyanov <hdobriyanov@gmail.com>
2018-01-03 15:05:01 +0200

    Updates

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    private bool grow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SizeModifier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SizeModifier : MonoBehaviour
+{
+    
+    private bool grow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 resize = transform.localScale;
+
+        if (grow)
+        {
+            resize = resize * 1.03f;
+        }
+        else
+        {
+            resize = resize * 0.97f;
+        }
+
+        if (resize.x > 5 || resize.x < 1)
+        {
+            grow = !grow;
+        }
+
+        transform.localScale = resize;
+    }
+}
